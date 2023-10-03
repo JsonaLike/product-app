38b6ec26a023b5d41893eae66358672e7c92cb3b
JsonaLike <ysfmohamed15@gmail.com>
2023-10-04 02:15:51 +0300

    test

--- src/app/product-list/product-list.component.ts
+++ src/app/product-list/product-list.component.ts
@@ -9,11 +9,23 @@ export class ProductListComponent {
   selectedFile: File | null = null;
 
   addProduct() {
-    this.product = {...this.newProduct};
-    
-    this.products.push(this.product); // Add the new product to the products array
+    if (this.selectedFile) {
+      const reader = new FileReader();
+      reader.onload = (event: any) => {
+        this.product = { ...this.newProduct, image: event.target.result };
+        this.products.push(this.product); // Add the new product to the products array
+      };
+      reader.readAsDataURL(this.selectedFile);
+
+    } else {
+      this.product = { ...this.newProduct, image: 'https://placehold.co/600x400' }; // Default image URL
+      this.products.push(this.product); // Add the new product to the products array
+
+    }
+
+    // Reset the selected file after adding the product
   }
-  newProduct: Product={id:34,name:'Product',price:23,description:'This is a Product Description'}; // Object to store new product data
+  newProduct: Product={id:34,name:'Product',price:23,description:'This is a Product Description',image:"null"}; // Object to store new product data
    onFileSelected(event: Event) {
     const inputElement = event.target as HTMLInputElement;
     if (inputElement.files && inputElement.files.length > 0) {
@@ -21,9 +33,9 @@ export class ProductListComponent {
     }
   }
   products = [
-    { id: 1, name: 'Product 1', price: 10 },
-    { id: 2, name: 'Product 2', price: 20 },
-    { id: 3, name: 'Product 3', price: 30 },
+    { id: 1, name: 'Product 1', price: 10 , image: 'https://placehold.co/600x400'},
+    { id: 2, name: 'Product 2', price: 20 , image: 'https://placehold.co/600x400'},
+    { id: 3, name: 'Product 3', price: 30 , image: 'https://placehold.co/600x400'},
     // Add more products as needed
     
   ];
@@ -34,6 +46,7 @@ export interface Product {
   id: number;
   name: string;
   price: number;
-  description:string
+  description:string,
+  image: string; 
   // Add more properties as needed
 }
